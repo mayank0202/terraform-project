3ecf061cf9230d10de45c44e3062462be27a9ca2
Mayank Sharma <83959396+mayank0202@users.noreply.github.com>
2022-03-03 10:49:27 +0530

    Delete modules/subnet directory

--- modules/subnet/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnet" {
-    value = aws_subnet.my_subnet1
-}
-
-output "route-table" {
-    value = aws_route_table.my-route-table
-}
--- modules/subnet/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "subnet_cidr_blocks" {}
-
-variable "avail_zone" {}
-
-variable "env_prefix" {}
-
-variable "vpc_id"{}
