8aabda11dab50a26a2ba0596d3bcf4c57c23c5db
mayank0202 <mayank.sharma@sourcefuse.com>
2022-10-12 21:47:58 +0530

    fix-2

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -12,20 +12,21 @@ resource "aws_security_group" "my-sg" {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["192.16.0.0/24"]
     }
 
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["192.168.1.0/24"]
         prefix_list_ids = []
     }
 
     tags = {
     Name : "${var.env_prefix}-sg"
   }
+  description = "security group"
 }
 
 data "aws_ami" "amazon-latest-ami-image" {
@@ -41,11 +42,18 @@ data "aws_ami" "amazon-latest-ami-image" {
 resource "aws_instance" "web-server" {
   ami = data.aws_ami.amazon-latest-ami-image.id
   instance_type = "t2.micro"
+  root_block_device {
+    encrypted = true
+  }
+  disable_api_termination = true
   subnet_id = var.subnet_id
   vpc_security_group_ids = [aws_security_group.my-sg.id]
   availability_zone = var.avail_zone
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   key_name = "mayank"
+  metadata_options {
+    http_tokens = required
+  }
 
   user_data = <<EOF
                #!/bin/bash
